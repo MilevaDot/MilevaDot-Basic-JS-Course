c3eff3fec6f447122b34b0e8cb6195a57f58aebf
Aaron Ilizarbe <aaron.d.ilizarbe.s@gmail.com>
2022-11-15 23:22:48 -0500

    operadores en javascript

---
+++ operators.js
@@ -0,0 +1,24 @@
+// Operadores binarios
+3+2;
+50-10;
+10*20;
+20/2;
+"Aaron " + "Ilizarbe" // el '+' concatena
+!false
+true
+var a = 1; // operador de asignación '='
+
+3 == "3" // true // Compara el valor en sí, no compara el tipo.
+3 === "3" // false // Compara todo, el valor y el tipo.
+
+5 < 3 // false
+5 > 3 // true
+5 <=6 // true
+5 >=6 // false
+a && b // Si a y b son verdaderos, sale true. Si alguno es falso, sale falso.
+a || b // Si a o b es true, sale verdadero.
+
+var edad = 40;
+edad++ // edad es 41
+
+edad += 2 // suma dos al valor edad.
