9418c78075718bad88261dbbdcf3923865fd3639
Aaron Ilizarbe <aaron.d.ilizarbe.s@gmail.com>
2022-11-15 22:54:22 -0500

    Una breve información de lo que es hoisting

---
+++ hoisting.js
@@ -0,0 +1,17 @@
+console.log(miNombre);
+var miNombre = "Aaron";
+// Esto retorna:
+// undefined
+// undefined
+
+
+
+hey();
+function hey(){
+    console.log("Hola " + miNombre);
+}
+var miNombre = "Aaron"
+
+// la función hey retornará:
+// Hola undefined
+// undefined
